5501f89ac6c546ee7261559172045dd8743315c9
albertospde <albertospde@gmail.com>
2026-04-28 16:47:32 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
-# giro-manager
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './GiroManager.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
